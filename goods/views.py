548ff3a21ff41a2bbfaa9197da13a324741a3b40
Artur Narinkov <a.narinkov@gmail.com>
2024-11-01 10:37:21 +0100

    The product filter on site page and urlencode

--- goods/views.py
+++ goods/views.py
@@ -6,11 +6,19 @@ from goods.models import Products
 
 def catalog(request, category_slug):
     page= request.GET.get('page', 1)
+    on_sale= request.GET.get('on_sale', None)
+    order_by= request.GET.get('order_by', None)
 
     if category_slug == "vse-tovary":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
@@ -25,7 +33,7 @@ def catalog(request, category_slug):
 
 def product(request, product_slug):
 
-    product = Products.objects.get(slug=product_slug)
+    products = Products.objects.get(slug=product_slug)
 
-    context = {"product": product}
+    context = {"product": products}
     return render(request, "goods/product.html", context=context)
