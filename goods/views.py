48f43f1320da98040d6aedca4ca3b65be1e82e71
Artur Narinkov <a.narinkov@gmail.com>
2024-11-01 12:09:22 +0100

    Simple search created

--- goods/views.py
+++ goods/views.py
@@ -2,22 +2,26 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
+from goods.utils import q_search
 
 
-def catalog(request, category_slug):
-    page= request.GET.get('page', 1)
-    on_sale= request.GET.get('on_sale', None)
-    order_by= request.GET.get('order_by', None)
+def catalog(request, category_slug=None):
+    page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
+    query = request.GET.get("q", None)
 
     if category_slug == "vse-tovary":
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     if on_sale:
         goods = goods.filter(discount__gt=0)
 
-    if order_by and order_by != 'default':
+    if order_by and order_by != "default":
         goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)
@@ -26,7 +30,7 @@ def catalog(request, category_slug):
     context = {
         "title": "Home - Каталог",
         "goods": current_page,
-        "slug_url": category_slug
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
